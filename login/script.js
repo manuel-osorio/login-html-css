8a0442b46359ac3519d88ce9d1d33bf53afc72de
manuel-osorio <manuel.osorio@ids.com.mx>
2022-11-02 13:24:17 -0600

    login

---
+++ login/script.js
@@ -0,0 +1,17 @@
+
+
+
+var  respuesta  =  new Array()
+var  Pregruntas  = new Array()
+respuesta = [1,2,3,4,5,6,7]
+Pregruntas =  [2,3,4,5,6,5,8]
+
+function igualdad (){
+    if(respuesta.length == Pregruntas.length){
+        console.log("son iguale ")
+       
+    }
+}
+
+
+console.log(igualdad())
